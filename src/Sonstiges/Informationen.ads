8d90d10c31ceccc989d8ec143d6fb69ad0f8f5b7
Honki Tonk <honki@dynwh-238-202.its.uni-kassel.de>
2020-12-09 01:17:33 +0100

    Spielerplatzierung überarbeitet und Cheatmenü eingefügt

--- src/Sonstiges/Informationen.ads
+++ src/Sonstiges/Informationen.ads
@@ -8,6 +8,6 @@ package Informationen is
 private
 
    Taste : Wide_Wide_Character;
-   Versionsnummer : Wide_Wide_String (1 .. 9) := "0.00.5910";
+   Versionsnummer : Wide_Wide_String (1 .. 9) := "0.00.5950";
 
 end Informationen;
