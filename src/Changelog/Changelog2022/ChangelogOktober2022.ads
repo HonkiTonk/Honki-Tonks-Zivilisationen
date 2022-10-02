7992f9173e2b0e4a9295b7123940b5478ea5a256
HonkiTonk <totalplanlos.de@web.de>
2022-10-03 01:13:32 +0200

    Satisch gelinkte Versionen aller Bibliotheken auf Basis der Entwicklungsdateien aus Debian Bullseye erstellt und getestet.

--- src/Changelog/Changelog2022/ChangelogOktober2022.ads
+++ src/Changelog/Changelog2022/ChangelogOktober2022.ads
@@ -196,18 +196,19 @@ package ChangelogOktober2022 is
    
    
 
-   -- Version 0.04.0660 => 0.04. (03.10.2022):
+   -- Version 0.04.0670 => 0.04. (03.10.2022):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.04.0595 => 0.04.0660 (02.10.2022):
+   -- Version 0.04.0595 => 0.04.0670 (02.10.2022):
    
    -- Diverse Spieletest unter Linux und Windows durchgeführt.
    -- Die Linux- und Windowsversion besser aufgeteilt.
-   -- Weiteres virtuelles System mit Debian Bullseye aufgesetzt um Bibliotheken mit älteren Versionen kompileiren zu können.
+   -- Weiteres virtuelles System mit Debian Bullseye aufgesetzt um Bibliotheken mit älteren Versionen kompilieren zu können.
+   -- Satisch gelinkte Versionen aller Bibliotheken auf Basis der Entwicklungsdateien aus Debian Bullseye erstellt und getestet.
    -- Mehrfaches, vollständiges Backup erstellt.
    -- Kommentare und Kleinigkeiten korrigiert.
    
