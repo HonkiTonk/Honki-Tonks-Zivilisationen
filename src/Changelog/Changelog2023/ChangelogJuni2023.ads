f2204e2b3a005066eccf770532e7fc30a2bf1b48
HonkiTonk <totalplanlos.de@web.de>
2023-06-13 09:06:44 +0200

    Fehler in den gpr Dateien korrigiert.

--- src/Changelog/Changelog2023/ChangelogJuni2023.ads
+++ src/Changelog/Changelog2023/ChangelogJuni2023.ads
@@ -119,16 +119,17 @@ package ChangelogVorlage is
    
    
 
-   -- Version 0.05.1900 => 0.05. (13.06.2023):
+   -- Version 0.05.1905 => 0.05. (13.06.2023):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05.1880 => 0.05.1900 (12..2023):
+   -- Version 0.05.1880 => 0.05.1910 (12..2023):
    
    -- GNAT-12 auf GNAT-13 aktualisiert und alle notwendigen Bibliotheken aktualisiert.
+   -- Fehler in den gpr Dateien korrigiert.
    
    
 
