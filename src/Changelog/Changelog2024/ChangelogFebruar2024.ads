4f8cd3acbe2d8a06839d6a146b7d84c59a6cc730
HonkiTonk <totalplanlos.de@web.de>
2024-01-31 21:49:15 +0100

    GNAT aktualisiert. VM aktualisiert.

--- src/Changelog/Changelog2024/ChangelogFebruar2024.ads
+++ src/Changelog/Changelog2024/ChangelogFebruar2024.ads
@@ -1,202 +1,202 @@
 package ChangelogFebruar2024 is
 
-   -- Version 0.0. => 0.0. (29.02.2024):
+   -- Version 0.05. => 0.05. (29.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (28.02.2024):
+   -- Version 0.05. => 0.05. (28.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (27.02.2024):
+   -- Version 0.05. => 0.05. (27.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (26.02.2024):
+   -- Version 0.05. => 0.05. (26.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (25.02.2024):
+   -- Version 0.05. => 0.05. (25.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (24.02.2024):
+   -- Version 0.05. => 0.05. (24.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (23.02.2024):
+   -- Version 0.05. => 0.05. (23.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (22.02.2024):
+   -- Version 0.05. => 0.05. (22.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (21.02.2024):
+   -- Version 0.05. => 0.05. (21.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (20.02.2024):
+   -- Version 0.05. => 0.05. (20.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (19.02.2024):
+   -- Version 0.05. => 0.05. (19.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (18.02.2024):
+   -- Version 0.05. => 0.05. (18.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (17.02.2024):
+   -- Version 0.05. => 0.05. (17.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (16.02.2024):
+   -- Version 0.05. => 0.05. (16.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (15.02.2024):
+   -- Version 0.05. => 0.05. (15.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (14.02.2024):
+   -- Version 0.05. => 0.05. (14.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (13.02.2024):
+   -- Version 0.05. => 0.05. (13.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (12.02.2024):
+   -- Version 0.05. => 0.05. (12.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (11.02.2024):
+   -- Version 0.05. => 0.05. (11.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (10.02.2024):
+   -- Version 0.05. => 0.05. (10.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (09.02.2024):
+   -- Version 0.05. => 0.05. (09.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (08.02.2024):
+   -- Version 0.05. => 0.05. (08.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (07.02.2024):
+   -- Version 0.05. => 0.05. (07.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (06.02.2024):
+   -- Version 0.05. => 0.05. (06.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (05.02.2024):
+   -- Version 0.05. => 0.05. (05.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (04.02.2024):
+   -- Version 0.05. => 0.05. (04.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (03.02.2024):
+   -- Version 0.05. => 0.05. (03.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (02.02.2024):
+   -- Version 0.05. => 0.05. (02.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (01.02.2024):
+   -- Version 0.05.5410 => 0.05. (01.02.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
--- src/Changelog/Changelog2024/ChangelogJanuar2024.ads
+++ src/Changelog/Changelog2024/ChangelogJanuar2024.ads
@@ -1,93 +1,81 @@
 package ChangelogJanuar2024 is
    
-   -- Version 0.05. => 0.05. (31.01.2024):
+   -- Version 0.05.5380 => 0.05.5410 (31.01.2024):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.05. => 0.05. (30.01.2024):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- GNAT aktualisiert.
+   -- VMs aktualisiert.
    
    
 
-   -- Version 0.05. => 0.05. (29.01.2024):
+   -- Version 0.05.5380 => 0.05.5380 (30.01.2024):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.05. => 0.05. (28.01.2024):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.05. => 0.05. (27.01.2024):
+   -- Version 0.05.5380 => 0.05.5380 (29.01.2024):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.05. => 0.05. (26.01.2024):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.05. => 0.05. (25.01.2024):
+   -- Version 0.05.5380 => 0.05.5380 (28.01.2024):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.05. => 0.05. (24.01.2024):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.05. => 0.05. (23.01.2024):
+   -- Version 0.05.5380 => 0.05.5380 (27.01.2024):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.05. => 0.05. (22.01.2024):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.05. => 0.05. (21.01.2024):
+   -- Version 0.05.5380 => 0.05.5380 (26.01.2024):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.05. => 0.05. (20.01.2024):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.05.5380 => 0.05. (19.01.2024):
+   -- Version 0.05.5380 => 0.05.5380 (25.01.2024):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
+   
+   
+
+   -- Version 0.05.5380 => 0.05.5380 (24.01.2024):
+   
+   -- null.
+   
+   
+
+   -- Version 0.05.5380 => 0.05.5380 (23.01.2024):
+   
+   -- null.
+   
+   
+
+   -- Version 0.05.5380 => 0.05.5380 (22.01.2024):
+   
+   -- null.
+   
+   
+
+   -- Version 0.05.5380 => 0.05.5380 (21.01.2024):
+   
+   -- null.
+   
+   
+
+   -- Version 0.05.5380 => 0.05.5380 (20.01.2024):
+   
+   -- null.
+   
+   
+
+   -- Version 0.05.5380 => 0.05.5380 (19.01.2024):
+   
+   -- null.
    
    
 
