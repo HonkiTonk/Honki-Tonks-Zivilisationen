2fe2e5357fe497c0bb0c32ecc0b3defe4952da64
HonkiTonk <totalplanlos.de@web.de>
2024-04-22 16:57:43 +0200

    Zu Testzwecken Arabisch als Sprache hinzugefügt. Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2024/ChangelogApril2024.ads
+++ src/Changelog/Changelog2024/ChangelogApril2024.ads
@@ -70,16 +70,16 @@ package ChangelogApril2024 is
    
    
 
-   -- Version 0.05. => 0.05. (20.04.2024):
+   -- Version 0.05.6410 => 0.05. (20.04.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05.6385 => 0.05. (19.04.2024):
+   -- Version 0.05.6385 => 0.05.6410 (19.04.2024):
    
-   -- 
+   -- Zu Testzwecken Arabisch als Sprache hinzugefügt.
    -- Kleinigkeiten korrigiert.
    
    
