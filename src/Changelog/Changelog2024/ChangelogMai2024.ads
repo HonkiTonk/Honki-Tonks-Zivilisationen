d31b91666fb9bfbb6c589f09c96a6b07b1ffda1f
HonkiTonk <totalplanlos.de@web.de>
2024-05-16 00:59:23 +0200

    Texturen überarbeitet und neu zusammengefasst. Unnötige Dateien gelöscht. Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2024/ChangelogMai2024.ads
+++ src/Changelog/Changelog2024/ChangelogMai2024.ads
@@ -119,16 +119,17 @@ package ChangelogMai2024 is
    
    
 
-   -- Version 0.05. => 0.05. (14.05.2024):
+   -- Version 0.05.7460 => 0.05. (14.05.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05.7425 => 0.05. (13.05.2024):
+   -- Version 0.05.7425 => 0.05.7460 (13.05.2024):
    
-   -- 
+   -- Texturen überarbeitet und neu zusammengefasst.
+   -- Unnötige Dateien gelöscht.
    -- Kleinigkeiten korrigiert.
    
    
