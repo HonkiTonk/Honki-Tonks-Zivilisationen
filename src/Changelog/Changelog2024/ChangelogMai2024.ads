6e442cea22f7975b8cd0e6d51c33e01a95e3dcc3
HonkiTonk <totalplanlos.de@web.de>
2024-05-19 17:08:29 +0200

    Texturen überarbeitet. Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2024/ChangelogMai2024.ads
+++ src/Changelog/Changelog2024/ChangelogMai2024.ads
@@ -84,21 +84,21 @@ package ChangelogMai2024 is
    
    
 
-   -- Version 0.05. => 0.05. (19.05.2024):
+   -- Version 0.05.7595 => 0.05. (19.05.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05. => 0.05. (18.05.2024):
+   -- Version 0.05.7570 => 0.05.7595 (18.05.2024):
    
-   -- 
+   -- Texturen überarbeitet.
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05.7545 => 0.05. (17.05.2024):
+   -- Version 0.05.7545 => 0.05.7570 (17.05.2024):
    
    -- Die ASFML Versionen vollständig zusammengefasst und direkt in das Projekt integriert.
    -- Kleinigkeiten korrigiert.
