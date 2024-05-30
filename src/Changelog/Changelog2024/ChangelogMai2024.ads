4e91f1843a080f63ff0d04356a135aa6ef08540b
HonkiTonk <totalplanlos.de@web.de>
2024-05-31 00:38:04 +0200

    Lokale Pragmas überarbeitet. Contracts und Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2024/ChangelogMai2024.ads
+++ src/Changelog/Changelog2024/ChangelogMai2024.ads
@@ -56,17 +56,17 @@ package ChangelogMai2024 is
    
    
 
-   -- Version 0.05. => 0.05. (23.05.2024):
+   -- Version 0.05.7715 => 0.05. (23.05.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05.7690 => 0.05. (22.05.2024):
+   -- Version 0.05.7690 => 0.05.7715 (22.05.2024):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- Lokale Pragmas überarbeitet.
+   -- Contracts und Kleinigkeiten korrigiert.
    
    
 
