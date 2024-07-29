1084f6c5a4ee97bf85e3d3fbefefd809f1f87bfb
HonkiTonk <totalplanlos.de@web.de>
2024-07-29 22:58:45 +0200

    Alire Pragmas an die Standard Pragmas angepasst. Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2024/ChangelogJuli2024.ads
+++ src/Changelog/Changelog2024/ChangelogJuli2024.ads
@@ -112,16 +112,16 @@ package ChangelogJuli2024 is
    
    
 
-   -- Version 0.05. => 0.05. (15.07.2024):
+   -- Version 0.05.8145 => 0.05. (15.07.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05.8120 => 0.05. (14.07.2024):
+   -- Version 0.05.8120 => 0.05.8145 (14.07.2024):
    
-   -- 
+   -- Alire Pragmas an die Standard Pragmas angepasst.
    -- Kleinigkeiten korrigiert.
    
    
