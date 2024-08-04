633d0ef2288fb4e4254ed7febbbf5b0e09334aff
HonkiTonk <totalplanlos.de@web.de>
2024-08-04 11:46:35 +0200

    gpr Dateien überarbeitet. Die Spielverzeichnisse für Linux und Windows zusammengeführt. Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2024/ChangelogJuli2024.ads
+++ src/Changelog/Changelog2024/ChangelogJuli2024.ads
@@ -84,16 +84,17 @@ package ChangelogJuli2024 is
    
    
 
-   -- Version 0.05. => 0.05. (19.07.2024):
+   -- Version 0.05.8315 => 0.05. (19.07.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05.8280 => 0.05. (18.07.2024):
+   -- Version 0.05.8280 => 0.05.8315 (18.07.2024):
    
-   -- 
+   -- gpr Dateien überarbeitet.
+   -- Die Spielverzeichnisse für Linux und Windows zusammengeführt.
    -- Kleinigkeiten korrigiert.
    
    
