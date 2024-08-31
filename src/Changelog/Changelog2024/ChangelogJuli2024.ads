caef3e141b7ab1d7224604f5a69b6bb3b3588a96
HonkiTonk <totalplanlos.de@web.de>
2024-08-31 20:23:40 +0200

    GNAT aktualisiert. Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2024/ChangelogJuli2024.ads
+++ src/Changelog/Changelog2024/ChangelogJuli2024.ads
@@ -7,16 +7,16 @@ package ChangelogJuli2024 is
    
    
 
-   -- Version 0.05. => 0.05. (30.07.2024):
+   -- Version 0.05.8720 => 0.05. (30.07.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05.8695 => 0.05. (29.07.2024):
+   -- Version 0.05.8695 => 0.05.8720 (29.07.2024):
    
-   -- 
+   -- GNAT aktualisiert.
    -- Kleinigkeiten korrigiert.
    
    
