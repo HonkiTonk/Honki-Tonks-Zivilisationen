f8e4cfcb1a842a6b2e72ee72bdf35049d614792a
HonkiTonk <totalplanlos.de@web.de>
2024-10-06 22:01:36 +0200

    Fonts aktualisiert. Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2024/ChangelogAugust2024.ads
+++ src/Changelog/Changelog2024/ChangelogAugust2024.ads
@@ -196,16 +196,16 @@ package ChangelogAugust2024 is
    
    
 
-   -- Version 0.05. => 0.05. (03.08.2024):
+   -- Version 0.05.8820 => 0.05. (03.08.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05.8795 => 0.05. (02.08.2024):
+   -- Version 0.05.8795 => 0.05.8820 (02.08.2024):
    
-   -- 
+   -- Fonts aktualisiert.
    -- Kleinigkeiten korrigiert.
    
    
--- src/Globales/GlobaleKonstanten/Datentypen/SonstigesKonstanten.ads
+++ src/Globales/GlobaleKonstanten/Datentypen/SonstigesKonstanten.ads
@@ -3,7 +3,7 @@ package SonstigesKonstanten is
 
    Spielname : constant Wide_Wide_String (1 .. 26) := "Honki Tonks Zivilisationen";
 
-   Versionsnummer : constant Wide_Wide_String (1 .. 9) := "0.05.8795";
+   Versionsnummer : constant Wide_Wide_String (1 .. 9) := "0.05.8820";
 
    SourceCode : constant Wide_Wide_String (1 .. 55) := "https://github.com/HonkiTonk/Honki-Tonks-Zivilisationen";
 
