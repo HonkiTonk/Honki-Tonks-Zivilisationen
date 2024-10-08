2548c6afda90cabcc94328da2561fbc5e5a68078
HonkiTonk <totalplanlos.de@web.de>
2024-10-08 23:16:35 +0200

    GPR Dateien angepasst. Veraltete Dateien entfernt. Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2024/ChangelogAugust2024.ads
+++ src/Changelog/Changelog2024/ChangelogAugust2024.ads
@@ -182,16 +182,17 @@ package ChangelogAugust2024 is
    
    
 
-   -- Version 0.05. => 0.05. (05.08.2024):
+   -- Version 0.05.8890 => 0.05. (05.08.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05.8855 => 0.05. (04.08.2024):
+   -- Version 0.05.8855 => 0.05.8890 (04.08.2024):
    
-   -- 
+   -- GPR Dateien angepasst.
+   -- Veraltete Dateien entfernt.
    -- Kleinigkeiten korrigiert.
    
    
