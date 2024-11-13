c3961786f135a8b7994145c9fe2663efa5f5ea22
HonkiTonk <totalplanlos.de@web.de>
2024-11-13 23:42:06 +0100

    Testmusik hinzugefügt. Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2024/ChangelogSeptember2024.ads
+++ src/Changelog/Changelog2024/ChangelogSeptember2024.ads
@@ -64,37 +64,34 @@ package ChangelogSeptember2024 is
    
    
 
-   -- Version 0.05. => 0.05. (21.09.2024):
+   -- Version 0.05.9580 => 0.05. (21.09.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05. => 0.05. (20.09.2024):
+   -- Version 0.05.9580 => 0.05.9580 (20.09.2024):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.05. => 0.05. (19.09.2024):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.05. => 0.05. (18.09.2024):
+   -- Version 0.05.9580 => 0.05.9580 (19.09.2024):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.05.9555 => 0.05. (17.09.2024):
+   -- Version 0.05.9580 => 0.05.9580 (18.09.2024):
    
-   -- 
+   -- null.
+   
+   
+
+   -- Version 0.05.9555 => 0.05.9580 (17.09.2024):
+   
+   -- Testmusik hinzugefügt.
    -- Kleinigkeiten korrigiert.
    
    
