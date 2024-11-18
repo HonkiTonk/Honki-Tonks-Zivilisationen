4984bb3019a251864574b590ebd9454ff248457e
HonkiTonk <totalplanlos.de@web.de>
2024-11-18 14:47:18 +0100

    Lizenzen aktualisiert/hinzugefügt. Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2024/ChangelogSeptember2024.ads
+++ src/Changelog/Changelog2024/ChangelogSeptember2024.ads
@@ -36,37 +36,34 @@ package ChangelogSeptember2024 is
    
    
 
-   -- Version 0.05. => 0.05. (25.09.2024):
+   -- Version 0.05.9605 => 0.05. (25.09.2024):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.05. => 0.05. (24.09.2024):
+   -- Version 0.05.9605 => 0.05.9605 (24.09.2024):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.05. => 0.05. (23.09.2024):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.05. => 0.05. (22.09.2024):
+   -- Version 0.05.9605 => 0.05.9605 (23.09.2024):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.05.9580 => 0.05. (21.09.2024):
+   -- Version 0.05.9605 => 0.05.9605 (22.09.2024):
    
-   -- 
+   -- null.
+   
+   
+
+   -- Version 0.05.9580 => 0.05.9605 (21.09.2024):
+   
+   -- Lizensen aktualisiert.
    -- Kleinigkeiten korrigiert.
    
    
