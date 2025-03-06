81f28132bffc542eae266873a26c0ec2bca1f2a0
HonkiTonk <totalplanlos.de@web.de>
2025-03-06 23:10:00 +0100

    Eigene Schließenprozedur eingebaut, um zusätzliche Debuginformationen zu bekommen und um zu verhindern dass versucht wird ungeöffnete Dateien zu schließen. Beim Einlesen der Steuerungsdatei wird jetzt geprüft ob eine Doppelbelegung vorliegt, was eventuell auftreten kann wenn die Steuerung erweitert wurde aber eine veraltete Steuerungsdatei verwendet wird. Einlesen der Font überarbeitet. Angefangen die Auswahl für Sprachen und Texturen in einem System zusammenzufügen. Contracts, Kommentare und Kleinigkeiten korrigiert/angepasst.

--- src/Changelog/Changelog2025/ChangelogMaerz2025.ads
+++ src/Changelog/Changelog2025/ChangelogMaerz2025.ads
@@ -1,217 +1,217 @@
 package ChangelogMaerz2025 is
    pragma Pure;
    
-   -- Version 0.0. => 0.0. (31.03.2025):
+   -- Version 0.06. => 0.06. (31.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (30.03.2025):
+   -- Version 0.06. => 0.06. (30.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (29.03.2025):
+   -- Version 0.06. => 0.06. (29.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (28.03.2025):
+   -- Version 0.06. => 0.06. (28.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (27.03.2025):
+   -- Version 0.06. => 0.06. (27.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (26.03.2025):
+   -- Version 0.06. => 0.06. (26.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (25.03.2025):
+   -- Version 0.06. => 0.06. (25.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (24.03.2025):
+   -- Version 0.06. => 0.06. (24.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (23.03.2025):
+   -- Version 0.06. => 0.06. (23.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (22.03.2025):
+   -- Version 0.06. => 0.06. (22.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (21.03.2025):
+   -- Version 0.06. => 0.06. (21.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (20.03.2025):
+   -- Version 0.06. => 0.06. (20.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (19.03.2025):
+   -- Version 0.06. => 0.06. (19.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (18.03.2025):
+   -- Version 0.06. => 0.06. (18.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (17.03.2025):
+   -- Version 0.06. => 0.06. (17.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (16.03.2025):
+   -- Version 0.06. => 0.06. (16.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (15.03.2025):
+   -- Version 0.06. => 0.06. (15.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (14.03.2025):
+   -- Version 0.06. => 0.06. (14.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (13.03.2025):
+   -- Version 0.06. => 0.06. (13.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (12.03.2025):
+   -- Version 0.06. => 0.06. (12.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (11.03.2025):
+   -- Version 0.06. => 0.06. (11.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (10.03.2025):
+   -- Version 0.06. => 0.06. (10.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (09.03.2025):
+   -- Version 0.06. => 0.06. (09.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (08.03.2025):
+   -- Version 0.06. => 0.06. (08.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (07.03.2025):
+   -- Version 0.06. => 0.06. (07.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (06.03.2025):
+   -- Version 0.06. => 0.06. (06.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (05.03.2025):
+   -- Version 0.06. => 0.06. (05.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (04.03.2025):
+   -- Version 0.06. => 0.06. (04.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (03.03.2025):
+   -- Version 0.06. => 0.06. (03.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (02.03.2025):
+   -- Version 0.06. => 0.06. (02.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (01.03.2025):
+   -- Version 0.06. => 0.06. (01.03.2025):
    
    -- 
    -- Kleinigkeiten korrigiert.
