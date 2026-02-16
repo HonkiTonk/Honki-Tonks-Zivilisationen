bb62170db3b82fcd03ca8b089906e34efdca5715
HonkiTonk <totalplanlos.de@web.de>
2026-02-16 11:24:49 +0100

    GNAT aktualisiert. Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2026/ChangelogFebruar2026.ads
+++ src/Changelog/Changelog2026/ChangelogFebruar2026.ads
@@ -1,198 +1,185 @@
 package ChangelogFebruar2026 is
    pragma Pure;
 
-   -- Version 0.0. => 0.0. (28.02.2026):
+   -- Version 0.06. => 0.06. (28.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (27.02.2026):
+   -- Version 0.06. => 0.06. (27.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (26.02.2026):
+   -- Version 0.06. => 0.06. (26.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (25.02.2026):
+   -- Version 0.06. => 0.06. (25.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (24.02.2026):
+   -- Version 0.06. => 0.06. (24.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (23.02.2026):
+   -- Version 0.06. => 0.06. (23.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (22.02.2026):
+   -- Version 0.06. => 0.06. (22.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (21.02.2026):
+   -- Version 0.06. => 0.06. (21.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (20.02.2026):
+   -- Version 0.06. => 0.06. (20.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (19.02.2026):
+   -- Version 0.06. => 0.06. (19.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (18.02.2026):
+   -- Version 0.06. => 0.06. (18.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (17.02.2026):
+   -- Version 0.06. => 0.06. (17.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (16.02.2026):
+   -- Version 0.06. => 0.06. (16.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (15.02.2026):
+   -- Version 0.06.3380 => 0.06. (15.02.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (14.02.2026):
+   -- Version 0.06.3355 => 0.06.3380 (14.02.2026):
    
-   -- 
+   -- GNAT aktualisiert.
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.0. => 0.0. (13.02.2026):
+   -- Version 0.06.3355 => 0.06.3355 (13.02.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.0. => 0.0. (12.02.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.0. => 0.0. (11.02.2026):
+   -- Version 0.06.3355 => 0.06.3355 (12.02.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.0. => 0.0. (10.02.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.0. => 0.0. (09.02.2026):
+   -- Version 0.06.3355 => 0.06.3355 (11.02.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.0. => 0.0. (08.02.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.0. => 0.0. (07.02.2026):
+   -- Version 0.06.3355 => 0.06.3355 (10.02.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.0. => 0.0. (06.02.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.0. => 0.0. (05.02.2026):
+   -- Version 0.06.3355 => 0.06.3355 (09.02.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.0. => 0.0. (04.02.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.0. => 0.0. (03.02.2026):
+   -- Version 0.06.3355 => 0.06.3355 (08.02.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.0. => 0.0. (02.02.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.0. => 0.0. (01.02.2026):
+   -- Version 0.06.3355 => 0.06.3355 (07.02.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (06.02.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (05.02.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (04.02.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (03.02.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (02.02.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (01.02.2026):
+   
+   -- null.
 
 end ChangelogFebruar2026;
--- src/Changelog/Changelog2026/ChangelogJanuar2026.ads
+++ src/Changelog/Changelog2026/ChangelogJanuar2026.ads
@@ -1,185 +1,159 @@
 package ChangelogJanuar2026 is
    pragma Pure;
    
-   -- Version 0.06. => 0.06. (31.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (31.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (30.01.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (29.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (30.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (28.01.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (27.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (29.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (26.01.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (25.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (28.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (24.01.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (23.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (27.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (22.01.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (21.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (26.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (20.01.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (19.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (25.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (18.01.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (17.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (24.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (16.01.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (15.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (23.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (14.01.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (13.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (22.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (12.01.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (11.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (21.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (10.01.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (09.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (20.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (08.01.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (07.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (19.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06.3355 => 0.06. (06.01.2026):
+   -- Version 0.06.3355 => 0.06.3355 (18.01.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (17.01.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (16.01.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (15.01.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (14.01.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (13.01.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (12.01.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (11.01.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (10.01.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (09.01.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (08.01.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (07.01.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3355 => 0.06.3355 (06.01.2026):
+   
+   -- null.
    
    
 
