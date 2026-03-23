14689d86eb541a312bbe3a7e262f1b6dbdf3e489
HonkiTonk <totalplanlos.de@web.de>
2026-03-23 01:59:07 +0100

    GPR Dateien angepasst. GNAT 14 aktualisiert. Kleinigkeiten korrigiert.

--- src/Changelog/Changelog2026/ChangelogMaerz2026.ads
+++ src/Changelog/Changelog2026/ChangelogMaerz2026.ads
@@ -57,136 +57,120 @@ package ChangelogMaerz2026 is
    
    
 
-   -- Version 0.06. => 0.06. (23.03.2026):
+   -- Version 0.06.3510 => 0.06. (23.03.2026):
    
    -- 
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.06. => 0.06. (22.03.2026):
+   -- Version 0.06.3475 => 0.06.3510 (22.03.2026):
    
-   -- 
+   -- GPR Dateien angepasst.
+   -- GNAT 14 aktualisiert.
    -- Kleinigkeiten korrigiert.
    
    
 
-   -- Version 0.06. => 0.06. (21.03.2026):
+   -- Version 0.06.3475 => 0.06.3475 (21.03.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (20.03.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (19.03.2026):
+   -- Version 0.06.3475 => 0.06.3475 (20.03.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (18.03.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (17.03.2026):
+   -- Version 0.06.3475 => 0.06.3475 (19.03.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (16.03.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (15.03.2026):
+   -- Version 0.06.3475 => 0.06.3475 (18.03.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (14.03.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (13.03.2026):
+   -- Version 0.06.3475 => 0.06.3475 (17.03.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (12.03.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (11.03.2026):
+   -- Version 0.06.3475 => 0.06.3475 (16.03.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (10.03.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (09.03.2026):
+   -- Version 0.06.3475 => 0.06.3475 (15.03.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (08.03.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06. => 0.06. (07.03.2026):
+   -- Version 0.06.3475 => 0.06.3475 (14.03.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
-   
-   
-
-   -- Version 0.06. => 0.06. (06.03.2026):
-   
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
    
    
 
-   -- Version 0.06.3475 => 0.06. (05.03.2026):
+   -- Version 0.06.3475 => 0.06.3475 (13.03.2026):
    
-   -- 
-   -- Kleinigkeiten korrigiert.
+   -- null.
+   
+   
+
+   -- Version 0.06.3475 => 0.06.3475 (12.03.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3475 => 0.06.3475 (11.03.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3475 => 0.06.3475 (10.03.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3475 => 0.06.3475 (09.03.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3475 => 0.06.3475 (08.03.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3475 => 0.06.3475 (07.03.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3475 => 0.06.3475 (06.03.2026):
+   
+   -- null.
+   
+   
+
+   -- Version 0.06.3475 => 0.06.3475 (05.03.2026):
+   
+   -- null.
    
    
 
